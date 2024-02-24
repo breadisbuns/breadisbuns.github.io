20f19fd2a2766d59c177c9f5da3a56613568a74b
Kevin Murphy <murph212@yahoo.com>
2024-02-24 09:38:28 -0500

    in progress

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transformIgnorePatterns: ["node_modules/(?!(sucrase)/)"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx|mjs)$": "babel-jest",
+  },
+};
